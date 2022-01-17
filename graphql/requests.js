035fe4bb57199e582373b9e19f33d23b836f2ffd
dalhickman <dalhickman@gmail.com>
2022-01-16 18:23:58 -0700

    Commit completed working btc-gql-api tutorial part 1

---
+++ graphql/requests.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+module.exports = {
+  getPrices: async () => {
+    const url = "https://blockchain.info/ticker";
+    try {
+      return await axios.get(url);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
